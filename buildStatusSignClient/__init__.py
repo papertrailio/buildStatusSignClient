44c2b66b98e4ecfc40a558a45a6fa4ede0bb382e
Rob Walker <rob@papertrail.io>
2018-07-14 22:28:42 +0100

    set mode first

--- buildStatusSignClient/__init__.py
+++ buildStatusSignClient/__init__.py
@@ -2,6 +2,9 @@ import RPi.GPIO as GPIO
 import time
 import pyrebase
 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
 OFF = 'OFF'
 ON = 'ON'
 FLASH = 'FLASH'
@@ -53,9 +56,6 @@ for key, project in pins.iteritems():
             print('pin ', color, value)
             GPIO.setup(value["pin"], GPIO.OUT)
             GPIO.output(value["pin"], GPIO.HIGH)
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
 
 config = {
     "authDomain": "buildstatussign.firebaseapp.com",
