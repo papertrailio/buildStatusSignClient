53e199bab24fe5a11179fcd39822b5d2da427b90
Rob Walker <rob@papertrail.io>
2018-07-14 21:42:45 +0100

    Testing firebase

--- buildStatusSignClient/__init__.py
+++ buildStatusSignClient/__init__.py
@@ -1,21 +1,41 @@
 import RPi.GPIO as GPIO
 import time
+import pyrebase
+
+pins = [7, 11, 12, 13, 15, 16, 18, 22, 23,
+        26, 29, 31, 33, 35, 36, 37, 38, 40]
+
+
+def stream_handler(message):
+    print(message["event"])  # put
+    print(message["path"])  # /-K7yGTTEp7O549EzTYtI
+    print(message["data"])  # {'title': 'Pyrebase', "body": "etc..."}
 
 
 def run():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
 
-    pins = [7, 11, 12, 13, 15, 16, 18, 22, 23,
-            26, 29, 31, 33, 35, 36, 37, 38, 40]
-
     for pin in pins:
         GPIO.setup(pin, GPIO.OUT)
-
-    for pin in pins:
-        GPIO.output(pin, GPIO.HIGH)
-
-    time.sleep(3)
-
-    for pin in pins:
         GPIO.output(pin, GPIO.LOW)
+
+    config = {
+        "databaseURL": "https://buildstatussign.firebaseio.com"
+    }
+
+    firebase = pyrebase.initialize_app(config)
+
+    db = firebase.database()
+
+    my_stream = db.child("statuses").stream(stream_handler)
+
+    # while True:
+
+    #     for pin in pins:
+    #         GPIO.output(pin, GPIO.HIGH)
+
+    #     time.sleep(3)
+
+    #     for pin in pins:
+    #         GPIO.output(pin, GPIO.LOW)
