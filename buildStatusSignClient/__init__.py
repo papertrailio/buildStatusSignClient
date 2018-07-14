b71fa6cc55855e469b51fd4a7fda2cff7801edbc
Rob Walker <rob@papertrail.io>
2018-07-14 21:33:19 +0100

    Add missing pin

--- buildStatusSignClient/__init__.py
+++ buildStatusSignClient/__init__.py
@@ -6,7 +6,8 @@ def run():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
 
-    pins = [7, 11, 12, 13, 15, 16, 18, 22, 23, 29, 31, 33, 35, 36, 37, 38, 40]
+    pins = [7, 11, 12, 13, 15, 16, 18, 22, 23,
+            26, 29, 31, 33, 35, 36, 37, 38, 40]
 
     for pin in pins:
         GPIO.setup(pin, GPIO.OUT)
