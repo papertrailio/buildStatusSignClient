f42bfe784c6ed1377c4ff121fd71a9576f699443
Rob Walker <rob@papertrail.io>
2018-07-14 23:09:54 +0100

    set green on success

--- buildStatusSignClient/__init__.py
+++ buildStatusSignClient/__init__.py
@@ -67,34 +67,46 @@ db = firebase.database()
 
 
 def switchGPIOs():
-    for key, projectData in pins.iteritems():
-        for build in projectData:
-            for key, colorState in build.iteritems():
-                if colorState["state"] == OFF:
-                    GPIO.output(colorState["pin"], GPIO.LOW)
-                else:
-                    GPIO.output(colorState["pin"], GPIO.HIGH)
+    while True:
+        for key, projectData in pins.iteritems():
+            for build in projectData:
+                for key, colorState in build.iteritems():
+                    if colorState["state"] == OFF:
+                        GPIO.output(colorState["pin"], GPIO.LOW)
+                    else:
+                        GPIO.output(colorState["pin"], GPIO.HIGH)
 
 
-def resetPinsForProject(projectName):
+def resetStateForProject(projectName):
     for build in pins[projectName]:
         for key, colorState in build.iteritems():
             print('color state', colorState)
             colorState["state"] = OFF
 
 
-def setPinStates(projectName, projectStates):
-    resetPinsForProject(projectName)
+def setStateForProjectBuild(buildPins, buildBuildState):
+    print("setStateForProjectBuild", buildBuildState)
+
+    if buildBuildState == 'success':
+        buildPins["green"]["state"] = ON
+    else:
+        buildPins["green"]["state"] = OFF
+
+
+def setStateForProjectBuilds(projectName, projectBuildStates):
+    setStateForProjectBuild(pins[projectName][0], projectBuildStates[0])
+    setStateForProjectBuild(pins[projectName][1], projectBuildStates[1])
+
+
+def setPinStates(projectName, projectBuilds):
+    resetStateForProject(projectName)
+    setStateForProjectBuilds(projectName, projectBuilds)
 
 
 def stream_handler(message):
     statuses = db.child("statuses").get()
     for project in statuses.each():
-        print(project.key())
-        print(project.val())
-        print('pins', pins)
         setPinStates(project.key(), project.val())
-        print('pins', pins)
 
 
 def streamData():
@@ -104,15 +116,4 @@ def streamData():
 def run():
     streamData()
 
-    while True:
-        switchGPIOs()
-
-    # while True:
-
-    #     for pin in pins:
-    #         GPIO.output(pin, GPIO.HIGH)
-
-    #     time.sleep(3)
-
-    #     for pin in pins:
-    #         GPIO.output(pin, GPIO.LOW)
+    switchGPIOs()
